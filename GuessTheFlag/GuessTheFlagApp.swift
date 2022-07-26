649f263444a213da04ccb6b2c30b84bba3214387
Daniel Allenby <dallenby@ford.com>
2022-07-26 16:50:39 +0100

    Initial commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Allenby, Daniel (D.A.) on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
